ec673d936c3b09414ab0208f369477f6e443d8c9
Afshin Teymoori <afshin_tm@yahoo.com>
2020-06-29 22:54:59 +1000

    renames

--- tools/docker/aws-cli-entry.sh
+++ tools/docker/aws-cli-entry.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 sleep 40
-aws --endpoint http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name tax-compliance-service-queue
-aws --endpoint http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name forms-engine-notifications-queue
-aws --endpoint http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name tos-notification-queue
-aws --endpoint-url http://local-stack:4575 sns create-topic --name compliance-notifications
-aws --endpoint-url http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name nz-compliance-settings-change-notifications-queue
-aws --endpoint-url http://local-stack:4575 sns subscribe --topic-arn arn:aws:sns:ap-southeast-2:000000000000:compliance-notifications --protocol sqs --notification-endpoint arn:aws:sns:ap-southeast-2:000000000000:nz-compliance-settings-change-notifications-queue --attributes "{\"RawMessageDelivery\":\"true\",\"FilterPolicy\":\"{\\\"event_type\\\":[\\\"NzClientComplianceSettingsChanged\\\"],\\\"region\\\":[\\\"NZ\\\"]}\"}"
-aws --endpoint-url http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name compliance-state-change-in-review-notifications-queue
-aws --endpoint-url http://local-stack:4575 sns subscribe --topic-arn arn:aws:sns:ap-southeast-2:000000000000:compliance-notifications --protocol sqs --notification-endpoint arn:aws:sns:ap-southeast-2:000000000000:compliance-state-change-in-review-notifications-queue --attributes "{\"RawMessageDelivery\":\"true\",\"FilterPolicy\":\"{\\\"event_type\\\":[\\\"ComplianceStateChangeToInReview\\\"],\\\"region\\\":[\\\"NZ\\\",\\\"AU\\\"]}\"}"
-aws --endpoint-url http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name compliance-state-change-lodge-notifications-queue
-aws --endpoint-url http://local-stack:4575 sns subscribe --topic-arn arn:aws:sns:ap-southeast-2:000000000000:compliance-notifications --protocol sqs --notification-endpoint arn:aws:sns:ap-southeast-2:000000000000:compliance-state-change-lodge-notifications-queue --attributes "{\"RawMessageDelivery\":\"true\",\"FilterPolicy\":\"{\\\"event_type\\\":[\\\"ComplianceStateChangeToLodged\\\"],\\\"region\\\":[\\\"NZ\\\",\\\"AU\\\"]}\"}"
-aws --endpoint-url http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name compliance-deleted-notifications-queue
-aws --endpoint-url http://local-stack:4575 sns subscribe --topic-arn arn:aws:sns:ap-southeast-2:000000000000:compliance-notifications --protocol sqs --notification-endpoint arn:aws:sns:ap-southeast-2:000000000000:compliance-deleted-notifications-queue --attributes "{\"RawMessageDelivery\":\"true\",\"FilterPolicy\":\"{\\\"event_type\\\":[\\\"ComplianceDeleted\\\"],\\\"region\\\":[\\\"NZ\\\",\\\"AU\\\"]}\"}"
+aws --endpoint http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name service1-queue
+aws --endpoint http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name service2-queue
+aws --endpoint http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name notification-queue
+aws --endpoint-url http://local-stack:4575 sns create-topic --name application-notifications
+aws --endpoint-url http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name communications-queue
+aws --endpoint-url http://local-stack:4575 sns subscribe --topic-arn arn:aws:sns:ap-southeast-2:000000000000:application-notifications --protocol sqs --notification-endpoint arn:aws:sns:ap-southeast-2:000000000000:communications-queue --attributes "{\"RawMessageDelivery\":\"true\",\"FilterPolicy\":\"{\\\"event_type\\\":[\\\"PersonSettingsChanged\\\"],\\\"region\\\":[\\\"NZ\\\"]}\"}"
+aws --endpoint-url http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name state-change-notifications-queue
+aws --endpoint-url http://local-stack:4575 sns subscribe --topic-arn arn:aws:sns:ap-southeast-2:000000000000:application-notifications --protocol sqs --notification-endpoint arn:aws:sns:ap-southeast-2:000000000000:state-change-notifications-queue --attributes "{\"RawMessageDelivery\":\"true\",\"FilterPolicy\":\"{\\\"event_type\\\":[\\\"StateChange\\\"],\\\"region\\\":[\\\"NZ\\\",\\\"AU\\\"]}\"}"
+aws --endpoint-url http://local-stack:4576 sqs create-queue --attributes file://aws-cli-attributes.json --queue-name person-deleted-notifications-queue
+aws --endpoint-url http://local-stack:4575 sns subscribe --topic-arn arn:aws:sns:ap-southeast-2:000000000000:application-notifications --protocol sqs --notification-endpoint arn:aws:sns:ap-southeast-2:000000000000:person-deleted-notifications-queue --attributes "{\"RawMessageDelivery\":\"true\",\"FilterPolicy\":\"{\\\"event_type\\\":[\\\"PersonDeleted\\\"],\\\"region\\\":[\\\"NZ\\\",\\\"AU\\\"]}\"}"
 aws --endpoint-url http://local-stack:4575 sns list-subscriptions
 sleep 1800
